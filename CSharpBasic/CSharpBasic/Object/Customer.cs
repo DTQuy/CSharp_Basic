694d03f1bd423cd15f5824f8c875c0d8f0bf4dbf
ADMIN <ADMIN@TQ23>
2023-11-13 23:25:22 +0700

    Update

---
+++ CSharpBasic/CSharpBasic/Object/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic.Object
+{
+    /// <summary>
+    /// Class Customer
+    /// </summary>
+    public class Customer
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        /// <summary>
+        /// Id Customer
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// FirstName Customer
+        /// </summary>
+        public string? FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// LastName Customer
+        /// </summary>
+        public string? LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Email User
+        /// </summary>
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// PhoneNumber User
+        /// </summary>
+        public string? PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Address
+        /// </summary>
+        public string? Address { get; set; }
+    }
+}
